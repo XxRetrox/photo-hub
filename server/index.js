21b9ec0d9bfd4758f6429aa87de141427a3b968b
Jesse <jasonreacher47@gmail.com>
2026-04-27 02:06:14 +0100

    Fix insecure connection to posgres

--- server/index.js
+++ server/index.js
@@ -24,6 +24,9 @@ const db = new pg.Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function testDbConnection() {
